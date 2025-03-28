17833418b79c464740322e4c78c7669f64e8793d
em1nal1yev <qabalaqamarvan2005@gmail.com>
2025-03-28 11:41:16 +0400

    yazdim ama yoxlamaga ureyim gelmedi

---
+++ mart28Task/Group.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart28Task
+{
+    class Group
+    {
+        public int groupNo;
+        public int studentLimit;
+        Student[] students = new Student[0];
+
+        public bool CheckGropupNo(int groupNo)
+        {
+            if(this.groupNo == groupNo)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddEmployee(Student e)
+        {
+            if (studentLimit > students.Length)
+            {
+                Student[] empp = new Student[students.Length + 1];
+                for (int i = 0; i < students.Length; i++)
+                {
+                    empp[i] = students[i];
+                }
+                for (int i = 0; i <= empp.Length; i++)
+                {
+                    if (i == students.Length)
+                    {
+                        empp[i] = e;
+                        students = empp;
+                        empp = students;
+                        break;
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine($"qoymaqcun yer qalmadi!!");
+            }
+        }
+
+        public void GetStudent(int id)
+        {
+            for(int i=0; i<students.Length; i++)
+            {
+                if(students[i].id == id)
+                {
+                    Console.WriteLine($"{students[id]}, {students[i].fullName}");
+                }
+            }
+        }
+
+        public void GetAllStudents()
+        {
+            for(int i = 0; i < students.Length; i++)
+            {
+                if (i == students.Length - 1)
+                {
+                    Console.WriteLine($"{students[i].id}, {students[i].fullName}, {students[i].point}.");
+                }
+                Console.Write($"{students[i].id}, {students[i].fullName}, {students[i].point} ;");
+            }
+        }
+
+    }
+}
---
+++ mart28Task/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mart28Task
+{
+    class Student
+    {
+        public int id;
+        public string fullName;
+        public int point;
+
+        public void StudentInfo()
+        {
+            Console.WriteLine($"idsi-{id}, fullName si-{fullName}, pointi-{point}");
+        }
+    }
+}
